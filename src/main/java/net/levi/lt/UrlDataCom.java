c3806df2be88f596668ff4739dd5675b868d0127
Levi Stacey <lastacey.dev@use.startmail.com>
2019-05-29 00:27:27 -0500

    Green Additonal UrlDataCom test

--- src/main/java/net/levi/lt/UrlDataCom.java
+++ src/main/java/net/levi/lt/UrlDataCom.java
@@ -1,10 +1,31 @@
 package net.levi.lt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class UrlDataCom {
 
-    public String urlGet (String url) {
+    public String urlGet(String url) throws MalformedURLException,IOException{
+        HttpsURLConnection uconn = (HttpsURLConnection) new URL(url).openConnection();
+        uconn.setRequestMethod("GET");
+        uconn.setUseCaches(false);  //Don't use cache.  Get it every time.
+        
+        BufferedReader buffreder = new BufferedReader(new InputStreamReader(uconn.getInputStream()));
 
-        return "";
+        StringBuilder strbuild = new StringBuilder();
+        String bufftemp = buffreder.readLine();
+
+        while(bufftemp != null){
+            strbuild.append(bufftemp);
+            bufftemp = buffreder.readLine();
+        }
+
+        return strbuild.toString();
     }
 
 }
--- src/test/java/net/levi/lt/UrlDataComTest.java
+++ src/test/java/net/levi/lt/UrlDataComTest.java
@@ -3,14 +3,47 @@ package net.levi.lt;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
 
-public class UrlDataComTest{
+public class UrlDataComTest {
 
-    @Test 
-    public void givenURL_ExpectsNotnull(){
+    @Test(expected = MalformedURLException.class)
+    public void givenBadURL_Throws() throws MalformedURLException, IOException {
         UrlDataCom urlDat = new UrlDataCom();
 
-        assertNotNull(urlDat.urlGet("")); 
+        assertNotNull(urlDat.urlGet(""));
 
     } 
+
+   @Test(expected = ConnectException.class)
+   public void givenMissDirectedURL_Throws() throws MalformedURLException, IOException {
+       UrlDataCom urlDat = new UrlDataCom();
+
+       assertNotNull(urlDat.urlGet("https://notavailidurl.busted"));
+
+   }  
+
+   @Test(expected = ClassCastException.class)
+   public void givenWrongSchemaURL_Throws() throws MalformedURLException, IOException {
+       UrlDataCom urlDat = new UrlDataCom();
+
+       assertNotNull(urlDat.urlGet("http://notavailidurl.busted"));
+   }  
+   
+   @Test
+   public void givenValidUrlWithBadAlbumId_ReturnsEmptyArray() throws MalformedURLException, IOException {
+       UrlDataCom urlDat = new UrlDataCom();
+       String tstring = urlDat.urlGet("https://jsonplaceholder.typicode.com/photos?albumId=99999");
+       assertEquals("[]",tstring);
+   }  
+
+      
+   @Test
+   public void givenValidUrlWithValidAlbumId_ReturnsValues() throws MalformedURLException, IOException {
+       UrlDataCom urlDat = new UrlDataCom();
+       String tstring = urlDat.urlGet("https://jsonplaceholder.typicode.com/photos?albumId=3").replace("\n", "").replace("\r", "");
+       assertTrue(tstring.startsWith("[  {    \"albumId\": 3,"));
+   }  
 }
